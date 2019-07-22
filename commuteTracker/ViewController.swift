d7059ea80ffd56f9e0487f5799aa8a538dc9f8ba
Kevin Veldkamp <kveldkamp@veyo.com>
2019-07-22 01:43:23 -0700

    Initial Commit

---
+++ commuteTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  commuteTracker
+//
+//  Created by Kevin Veldkamp on 7/22/19.
+//  Copyright © 2019 kevin veldkamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
